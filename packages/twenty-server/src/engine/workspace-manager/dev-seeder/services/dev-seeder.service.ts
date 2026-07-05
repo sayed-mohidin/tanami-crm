56756516a95ec1d58aa258044e09201a63f7435a
Sayed Mohidin <sayed_mohidin@hotmail.com>
2026-07-05 12:18:25 +0300

    Make Tanami seed schema only

--- packages/twenty-server/src/engine/workspace-manager/dev-seeder/services/dev-seeder.service.ts
+++ packages/twenty-server/src/engine/workspace-manager/dev-seeder/services/dev-seeder.service.ts
@@ -74,9 +74,10 @@ export class DevSeederService {
 
   public async seedDev(
     workspaceId: SeededWorkspacesIds,
-    options?: { light?: boolean },
+    options?: { light?: boolean; metadataOnly?: boolean },
   ): Promise<void> {
     const light = options?.light ?? false;
+    const metadataOnly = options?.metadataOnly ?? false;
     const isBillingEnabled = this.twentyConfigService.get('IS_BILLING_ENABLED');
     const appVersion = this.twentyConfigService.get('APP_VERSION') ?? 'unknown';
 
@@ -182,12 +183,14 @@ export class DevSeederService {
         this.workspaceMigrationValidateBuildAndRunService,
     });
 
-    await this.devSeederDataService.seed({
-      schemaName,
-      workspaceId,
-      featureFlags: featureFlagsMap,
-      light,
-    });
+    if (!metadataOnly) {
+      await this.devSeederDataService.seed({
+        schemaName,
+        workspaceId,
+        featureFlags: featureFlagsMap,
+        light,
+      });
+    }
 
     await this.workspaceCacheStorageService.flush(workspaceId, undefined);
   }
